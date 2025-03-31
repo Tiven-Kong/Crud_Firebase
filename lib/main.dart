5d03ca9774b86eb0f875c9d000fbff279b9725ae
Ven <tivoenkong36@gmail.com>
2025-03-31 21:45:59 +0700

    Add New Item and Delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// REPOSITORY
 import 'dart:convert';
 import 'dart:io';
 
@@ -9,174 +8,248 @@ import 'package:provider/provider.dart';
 import 'async_value.dart';
 
 // REPOS
-abstract class PancakeRepository {
-  Future<Pancake> addPancake({required String color, required double price});
-  Future<List<Pancake>> getPancakes();
+abstract class BookRepository {
+  Future<Book> addBook(
+      {required String author_name, required String title, required int year});
+  Future<List<Book>> getBooks();
+  Future<void> deleteBook(String bookId);
 }
 
-class FirebasePancakeRepository extends PancakeRepository {
-  static const String baseUrl = 'YOUR URL';
-  static const String pancakesCollection = "pancakes";
-  static const String allPancakesUrl = '$baseUrl/$pancakesCollection.json';
+class FirebaseBooksRepository extends BookRepository {
+  static const String baseUrl =
+      'https://firbasestart-d97e4-default-rtdb.asia-southeast1.firebasedatabase.app';
+  static const String bookCollection = "books";
+  static const String allBooksUrl = '$baseUrl/$bookCollection.json';
 
   @override
-  Future<Pancake> addPancake({required String color, required double price}) async {
-    Uri uri = Uri.parse(allPancakesUrl);
+  Future<Book> addBook(
+      {required String author_name, required String title, required int year}) async {
+    Uri uri = Uri.parse(allBooksUrl);
 
-    // Create a new data
-    final newPancakeData = {'color': color, 'price': price};
+    // Create new data
+    final newBookData = {'author': author_name, 'title': title, 'year': year};
     final http.Response response = await http.post(
       uri,
       headers: {'Content-Type': 'application/json'},
-      body: json.encode(newPancakeData),
+      body: json.encode(newBookData),
     );
 
     // Handle errors
     if (response.statusCode != HttpStatus.ok) {
-      throw Exception('Failed to add user');
+      throw Exception('Failed to add book');
     }
 
     // Firebase returns the new ID in 'name'
     final newId = json.decode(response.body)['name'];
 
-    // Return created user
-    return Pancake(id: newId, color: color, price: price);
+    // Return created book
+    return Book(id: newId, author_name: author_name, title: title, year: year);
   }
 
   @override
-  Future<List<Pancake>> getPancakes() async {
-    Uri uri = Uri.parse(allPancakesUrl);
+  Future<List<Book>> getBooks() async {
+    Uri uri = Uri.parse(allBooksUrl);
     final http.Response response = await http.get(uri);
 
     // Handle errors
-    if (response.statusCode != HttpStatus.ok && response.statusCode != HttpStatus.created) {
+    if (response.statusCode != HttpStatus.ok &&
+        response.statusCode != HttpStatus.created) {
       throw Exception('Failed to load');
     }
 
-    // Return all users
+    // Return all books
     final data = json.decode(response.body) as Map<String, dynamic>?;
 
     if (data == null) return [];
-    return data.entries.map((entry) => PancakeDto.fromJson(entry.key, entry.value)).toList();
+    return data.entries
+        .map((entry) => BookDto.fromJson(entry.key, entry.value))
+        .toList();
+  }
+
+  @override
+  Future<void> deleteBook(String bookId) async {
+    try {
+      Uri uri = Uri.parse('$baseUrl/$bookCollection/$bookId.json');
+      final http.Response response = await http.delete(uri);
+      if (response.statusCode == 200) {
+        print("DeleteSuccess");
+        getBooks();
+      }
+      if (response.statusCode != HttpStatus.ok) {
+        throw Exception('Failed to delete book');
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
   }
 }
 
-class MockPancakeRepository extends PancakeRepository {
-  final List<Pancake> pancakes = [];
+class MockBookRepository extends BookRepository {
+  final List<Book> books = [];
 
   @override
-  Future<Pancake> addPancake({required String color, required double price}) {
+  Future<Book> addBook(
+      {required String author_name, required String title, required int year}) {
     return Future.delayed(Duration(seconds: 1), () {
-      Pancake newPancake = Pancake(id: "0", color: color, price: 12);
-      pancakes.add(newPancake);
-      return newPancake;
+      Book newBook = Book(
+          id: "0", author_name: author_name, title: title, year: year);
+      books.add(newBook);
+      return newBook;
     });
   }
 
   @override
-  Future<List<Pancake>> getPancakes() {
-    return Future.delayed(Duration(seconds: 1), () => pancakes);
+  Future<List<Book>> getBooks() {
+    return Future.delayed(Duration(seconds: 1), () => books);
+  }
+
+  @override
+  Future<void> deleteBook(String bookId) {
+    books.removeWhere((book) => book.id == bookId);
+    return Future.value();
   }
 }
 
 // MODEL & DTO
-class PancakeDto {
-  static Pancake fromJson(String id, Map<String, dynamic> json) {
-    return Pancake(id: id, color: json['color'], price: json['price']);
+class BookDto {
+  static Book fromJson(String id, Map<String, dynamic> json) {
+    return Book(
+        id: id,
+        author_name: json['author'],
+        title: json['title'],
+        year: json['year']);
   }
 
-  static Map<String, dynamic> toJson(Pancake pancake) {
-    return {'name': pancake.color, 'price': pancake.price};
+  static Map<String, dynamic> toJson(Book book) {
+    return {
+      'author': book.author_name,
+      'title': book.title,
+      'year': book.year
+    };
   }
 }
 
 // MODEL
-class Pancake {
+class Book {
   final String id;
-  final String color;
-  final double price;
+  final String author_name;
+  final String title;
+  final int year;
 
-  Pancake({required this.id, required this.color, required this.price});
+  Book(
+      {required this.id,
+        required this.author_name,
+        required this.title,
+        required this.year});
 
   @override
   bool operator ==(Object other) {
-    return other is Pancake && other.id == id;
+    return other is Book && other.id == id;
   }
 
   @override
   int get hashCode => super.hashCode ^ id.hashCode;
 }
 
-// PROVIDER
-class Pancakeprovider extends ChangeNotifier {
-  final PancakeRepository _repository;
-  AsyncValue<List<Pancake>>? pancakesState;
 
-  Pancakeprovider(this._repository) {
-    fetchUsers();
+
+
+// PROVIDER
+class BookProvider extends ChangeNotifier {
+  final BookRepository _repository;
+  AsyncValue<List<Book>>? booksState;
+
+  BookProvider(this._repository) {
+    fetchBooks();
   }
 
-  bool get isLoading => pancakesState != null && pancakesState!.state == AsyncValueState.loading;
-  bool get hasData => pancakesState != null && pancakesState!.state == AsyncValueState.success;
+  bool get isLoading =>
+      booksState != null && booksState!.state == AsyncValueState.loading;
 
-  void fetchUsers() async {
+  bool get hasData =>
+      booksState != null && booksState!.state == AsyncValueState.success;
+
+  void fetchBooks() async {
     try {
       // 1- loading state
-      pancakesState = AsyncValue.loading();
+      booksState = AsyncValue.loading();
       notifyListeners();
 
-      // 2 - Fetch users
-      pancakesState = AsyncValue.success(await _repository.getPancakes());
+      // 2 - Fetch books
+      booksState = AsyncValue.success(await _repository.getBooks());
 
-      print("SUCCESS: list size ${pancakesState!.data!.length.toString()}");
+      print("FetchData");
 
       // 3 - Handle errors
     } catch (error) {
       print("ERROR: $error");
-      pancakesState = AsyncValue.error(error);
+      booksState = AsyncValue.error(error);
     }
 
     notifyListeners();
   }
 
-  void addPancake(String color, double price) async {
+  void addBook(String author_name, String title, int year) async {
     // 1- Call repo to add
-    _repository.addPancake(color: color, price: price);
+    await _repository.addBook(
+        author_name: author_name, title: title, year: year);
+   print("Add SUCCESS");
+    // 2- Call repo to fetch
+    fetchBooks();
+  }
+
+  void deleteBook(String bookId) async {
+
+    await _repository.deleteBook(bookId);
 
     // 2- Call repo to fetch
-    fetchUsers();
+    fetchBooks();
   }
 }
+
+
+
 
 class App extends StatelessWidget {
   const App({super.key});
 
   void _onAddPressed(BuildContext context) {
-    final Pancakeprovider pancakeProvider = context.read<Pancakeprovider>();
-    pancakeProvider.addPancake("blue", 3.1);
+    final BookProvider bookProvider = context.read<BookProvider>();
+    bookProvider.addBook("New Author", "New Title", 2025);
   }
 
   @override
   Widget build(BuildContext context) {
-    final pancakeProvider = Provider.of<Pancakeprovider>(context);
+    final bookProvider = Provider.of<BookProvider>(context);
 
     Widget content = Text('');
-    if (pancakeProvider.isLoading) {
+    if (bookProvider.isLoading) {
       content = CircularProgressIndicator();
-    } else if (pancakeProvider.hasData) {
-      List<Pancake> pancakes = pancakeProvider.pancakesState!.data!;
+    } else if (bookProvider.hasData) {
+      List<Book> books = bookProvider.booksState!.data!;
 
-      if (pancakes.isEmpty) {
+      if (books.isEmpty) {
         content = Text("No data yet");
       } else {
         content = ListView.builder(
-          itemCount: pancakes.length,
-          itemBuilder:
-              (context, index) => ListTile(
-                title: Text(pancakes[index].color),
-                subtitle: Text("${pancakes[index].price}"),
-                trailing: IconButton(icon: Icon(Icons.delete, color: Colors.red), onPressed: () => {}),
-              ),
+          itemCount: books.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(books[index].title),
+            subtitle: Row(
+              children: [
+                Text("${books[index].author_name}"),
+                Text("${books[index].year}"),
+              ],
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.delete, color: Colors.red),
+              onPressed: () {
+                final book = books[index];
+                final BookProvider bookProvider = context.read<BookProvider>();
+                bookProvider.deleteBook(book.id);
+              },
+            ),
+          ),
         );
       }
     }
@@ -184,7 +257,11 @@ class App extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
-        actions: [IconButton(onPressed: () => _onAddPressed(context), icon: const Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: () => _onAddPressed(context),
+              icon: const Icon(Icons.add))
+        ],
       ),
       body: Center(child: content),
     );
@@ -194,12 +271,12 @@ class App extends StatelessWidget {
 // 5 - MAIN
 void main() async {
   // 1 - Create repository
-  final PancakeRepository pancakeRepository = FirebasePancakeRepository();
+  final BookRepository bookRepository = FirebaseBooksRepository();
 
   // 2-  Run app
   runApp(
     ChangeNotifierProvider(
-      create: (context) => Pancakeprovider(pancakeRepository),
+      create: (context) => BookProvider(bookRepository),
       child: MaterialApp(debugShowCheckedModeBanner: false, home: const App()),
     ),
   );
